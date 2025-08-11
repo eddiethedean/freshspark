75dc95066be23e65adbb7e0f2d510ccaa197e90c
Odos Matthews <omatth10@wgu.edu>
2025-08-11 13:03:36 -0400

    first commit

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from freshspark import reset_active_session
+
+@pytest.fixture(autouse=True)
+def _ensure_no_active_session_before_after():
+    # Make sure nothing is hanging around before/after each test
+    reset_active_session()
+    yield
+    reset_active_session()
